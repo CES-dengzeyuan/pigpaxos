ffca7553957c1a43aaf6053a223e4f75513bca6e
CES-dengzeyuan <2101704@stu.neu.edu.cn>
2023-11-13 08:08:06 +0000

    fix create config

--- bin/monitor.py
+++ bin/monitor.py
@@ -6,83 +6,50 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import json
+import sys
+import os
 
-
-with open('config.json', 'r') as file:
-    config = json.load(file)
-
-address_info = config['address']
-servers = [
-    {
-        'hostname': f'server{i+1}',
-        'hostip': address.split('//')[1].split(':')[0],
-        'port': 22,
-        'username': 'root',
-        'password': 'Aa123456'
-    }
-    for i, address in enumerate(address_info.values())
-]
-
-# servers = [
-#     {'hostname': 'server1', 'hostip': '172.24.212.230', 'port': 22, 'username': 'root', 'password': 'Aa123456'},
-#     {'hostname': 'server2', 'hostip': '172.24.212.231', 'port': 22, 'username': 'root', 'password': 'Aa123456'},
-#     {'hostname': 'server3', 'hostip': '172.24.212.232', 'port': 22, 'username': 'root', 'password': 'Aa123456'},
-# ]
-
-duration_minutes = 1
-frequency_seconds = 5
-total_samples = (duration_minutes * 60) // frequency_seconds
-
+# Global variables for storing data
 cpu_data = defaultdict(list)
 memory_data = defaultdict(list)
 data_lock = threading.Lock()
 
 
-def monitor_server(server):
+def monitor_server(server, total_samples, frequency_seconds):
+    """Function to monitor server's CPU and memory usage."""
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(server['hostip'], server['port'], server['username'], server['password'])
+
     try:
         for _ in range(total_samples):
+            # Command to get CPU usage
             stdin, stdout, stderr = client.exec_command("top -bn1 | grep 'Cpu(s)' | awk '{print $2+$4}'")
             cpu_usage = float(stdout.read().decode().strip())
+
+            # Command to get memory usage
             stdin, stdout, stderr = client.exec_command("free | grep Mem | awk '{print $3/$2 * 100.0}'")
             memory_usage = float(stdout.read().decode().strip())
 
             with data_lock:
                 cpu_data[server['hostname']].append(cpu_usage)
                 memory_data[server['hostname']].append(memory_usage)
+
             time.sleep(frequency_seconds)
+
     finally:
         client.close()
 
 
-threads = []
-for server in servers:
-    thread = threading.Thread(target=monitor_server, args=(server,))
-    thread.start()
-    threads.append(thread)
-
-for thread in threads:
-    thread.join()
-
-with data_lock:
-    cpu_usage_df = pd.DataFrame(cpu_data)
-    memory_usage_df = pd.DataFrame(memory_data)
-    cpu_usage_df.to_csv('cpu_usage.csv', index=False)
-    memory_usage_df.to_csv('memory_usage.csv', index=False)
-
-
 def plot_heatmap(csv_file, title, pdf_file):
+    """Function to plot heatmap from CSV data."""
     df = pd.read_csv(csv_file).T
-
     sorted_index = sorted(df.index, key=lambda x: int(x.replace('server', '')))
     df = df.reindex(sorted_index)
 
     plt.figure(figsize=(10, 8))
     cmap = sns.diverging_palette(230, 20, as_cmap=True)
     sns.heatmap(df, annot=False, cmap=cmap, square=True)
-    # sns.heatmap(df, annot=False, cmap="viridis", square=True)
     plt.title(title)
     plt.ylabel('Server')
     plt.xlabel('Sampling Point')
@@ -92,7 +59,61 @@ def plot_heatmap(csv_file, title, pdf_file):
     plt.savefig(pdf_file, format='pdf')
     plt.close()
 
-sns.set_theme()
 
-plot_heatmap('cpu_usage.csv', 'CPU Usage Heatmap', 'cpu_usage_heatmap.pdf')
-plot_heatmap('memory_usage.csv', 'Memory Usage Heatmap', 'memory_usage_heatmap.pdf')
+def main():
+    """Main function to initiate server monitoring and plotting."""
+    # Check for command line arguments
+    if len(sys.argv) < 2:
+        print("Usage: python3 monitor.py <log_directory>")
+        sys.exit(1)
+
+    log_directory = sys.argv[1]
+    os.makedirs(log_directory, exist_ok=True)
+
+    with open('config.json', 'r') as file:
+        config = json.load(file)
+
+    address_info = config['address']
+    servers = [
+        {
+            'hostname': f'server{i + 1}',
+            'hostip': address.split('//')[1].split(':')[0],
+            'port': 22,
+            'username': 'root',
+            'password': 'Aa123456'
+        }
+        for i, address in enumerate(address_info.values())
+    ]
+
+    duration_minutes = 1
+    frequency_seconds = 5
+    total_samples = (duration_minutes * 60) // frequency_seconds
+
+    threads = []
+    for server in servers:
+        thread = threading.Thread(target=monitor_server, args=(server, total_samples, frequency_seconds))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+    cpu_usage_csv = os.path.join(log_directory, 'cpu_usage.csv')
+    memory_usage_csv = os.path.join(log_directory, 'memory_usage.csv')
+    cpu_usage_pdf = os.path.join(log_directory, 'cpu_usage_heatmap.pdf')
+    memory_usage_pdf = os.path.join(log_directory, 'memory_usage_heatmap.pdf')
+
+    with data_lock:
+        cpu_usage_df = pd.DataFrame(cpu_data)
+        memory_usage_df = pd.DataFrame(memory_data)
+        cpu_usage_df.to_csv(cpu_usage_csv, index=False)
+        memory_usage_df.to_csv(memory_usage_csv, index=False)
+
+    sns.set_theme()
+    plot_heatmap(cpu_usage_csv, 'CPU Usage Heatmap', cpu_usage_pdf)
+    plot_heatmap(memory_usage_csv, 'Memory Usage Heatmap', memory_usage_pdf)
+
+
+if __name__ == "__main__":
+    main()
+
--- bin/remote.py
+++ bin/remote.py
@@ -1,4 +1,6 @@
 import json
+import math
+
 import paramiko
 import threading
 import subprocess
@@ -10,16 +12,16 @@ def read_config(file_path):
         return json.load(file)
 
 
-def get_extra_args(algorithm, pg, fr):
+def get_extra_args(algorithm, pg, nodes):
     if algorithm == "pigpaxos":
-        return f"-pg {pg} -fr {str(fr).lower()} "
+        return f"-algorithm={algorithm} -pg {pg} "
     elif algorithm == "layerpaxos":
-        return f"-npg {pg} -wfr {str(fr).lower()} "
+        return f"-algorithm=pigpaxos -pg {pg} -rgslack {nodes} "
     else:
         return ""
 
 
-def start_service(server_info, algorithm, log_dir, extra_args):
+def start_service(server_info, log_dir, extra_args):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
@@ -29,7 +31,7 @@ def start_service(server_info, algorithm, log_dir, extra_args):
                     password=server_info['password'])
         cmd = f"cd /root/pigpaxos/bin; mkdir {log_dir}"
         ssh.exec_command(cmd)
-        cmd = f"cd /root/pigpaxos/bin; ./server -id {server_info['id']} -log_dir {log_dir} -algorithm={algorithm} {extra_args}&"
+        cmd = f"cd /root/pigpaxos/bin; ./server -id {server_info['id']} -log_dir {log_dir} {extra_args}&"
         ssh.exec_command(cmd)
     finally:
         ssh.close()
@@ -57,7 +59,8 @@ def kill_remote_processes(slpconf):
 
 
 def start_local_processes(log_dir, config_path, slpconf):
-    monitor_cmd = "python3 monitor.py &"
+    monitor_cmd = f"python3 monitor.py {log_dir} &"
+    print(monitor_cmd)
     subprocess.Popen(monitor_cmd, shell=True)
 
     client_cmd = f"./client -id 1.1 -log_dir {log_dir} -config {config_path}"
@@ -69,41 +72,49 @@ def start_local_processes(log_dir, config_path, slpconf):
 
 if __name__ == "__main__":
     config_path = 'config.json'
-    algorithm = "paxos"
-    pg = 1
-    fr = True
+    algorithms = ["paxos", "epaxos", "pigpaxos", "layerpaxos"]
     username = "root"
     passwd = "Aa123456"
 
     config = read_config(config_path)
-    extra_args = get_extra_args(algorithm, pg, fr)
+    IPLen = len(config['address'].items())
+    pg = int(math.sqrt(IPLen))
+    nodes = int(IPLen / (pg * 2))
 
-    log_dir = f"{algorithm}-{len(config['address'])}-{pg}"
-    os.makedirs(log_dir, exist_ok=True)
+    for algorithm in algorithms:
+        if algorithm == "paxos":
+            extra_args = f"-algorithm={algorithm} "
+        elif algorithm == "epaxos":
+            extra_args = f"-algorithm={algorithm} "
+        else:
+            extra_args = get_extra_args(algorithm, pg, nodes)
 
-    threads = []
-    for server_id, addr in config['address'].items():
-        hostIP, port = addr.replace('tcp://', '').split(':')
-        server_info = {
-            'id': server_id,
-            'ip': hostIP,
-            'port': 22,
+        log_dir = f"{algorithm}-{IPLen}"
+        os.makedirs(log_dir, exist_ok=True)
+
+        threads = []
+        for server_id, addr in config['address'].items():
+            hostIP, port = addr.replace('tcp://', '').split(':')
+            server_info = {
+                'id': server_id,
+                'ip': hostIP,
+                'port': 22,
+                'username': username,
+                'password': passwd,
+            }
+            thread = threading.Thread(target=start_service,
+                                      args=(server_info, log_dir, extra_args))
+            thread.start()
+            threads.append(thread)
+
+        for thread in threads:
+            thread.join()
+
+        slpconf = {
+            'config': config['address'].items(),
             'username': username,
             'password': passwd,
         }
-        thread = threading.Thread(target=start_service,
-                                  args=(server_info, algorithm, log_dir, extra_args))
-        thread.start()
-        threads.append(thread)
+        start_local_processes(log_dir, config_path, slpconf)
 
-    for thread in threads:
-        thread.join()
-
-    slpconf = {
-        'config': config['address'].items(),
-        'username': username,
-        'password': passwd,
-    }
-    start_local_processes(log_dir, config_path, slpconf)
-
-    print("所有服务器和客户端已启动")
+        print("所有服务器和客户端已启动")
